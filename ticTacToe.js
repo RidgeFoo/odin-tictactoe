e3426e49acf74d63de6101f36c58d4a8d112d1d2
RidgeFoo <m.pagnol@pm.me>
2022-05-18 15:31:47 +0100

    Adds rendering of grid

--- ticTacToe.js
+++ ticTacToe.js
@@ -5,11 +5,9 @@ As little as possible in Global State
   - Array for the tic tac toe.
   - Must control the flow of the game itself.
 2. Player factory function
-  - 
 3. displayController module
 
-
-Render the contents of the gameBoard array to the page.
+✅ Render the contents of the gameBoard array to the page.
   - Manually fill in the X's and O's for now
   - Use a 3x3 Grid
 
@@ -26,3 +24,39 @@ Allow players to put in their names.
 Include a start / restart the game button.
 Display element that congratulates the winner
 */
+
+const gameBoard = (function () {
+  const grid = [null, null, null, null, null, null, null, null, null];
+
+  function updateGrid(index, player = "X") {
+    if (!grid[index]) {
+      grid.splice(index, 1, player);
+    }
+  }
+  return { grid, updateGrid };
+})();
+
+const displayController = (function () {
+  const gameBoardGrid = document.querySelectorAll(".box");
+
+  // Setup Event Listeners
+  gameBoardGrid.forEach((box, index) => {
+    box.addEventListener("click", () => {
+      gameBoard.updateGrid(index);
+      render();
+    });
+  });
+
+  // Render the game grid
+  function render() {
+    gameBoardGrid.forEach((box, index) => {
+      box.textContent = gameBoard.grid[index];
+    });
+  }
+
+  return { render };
+})();
+
+function Player(name) {
+  return { name };
+}
